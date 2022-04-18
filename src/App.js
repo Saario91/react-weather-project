e6eae69e32455f81f4d68468e72ffad381bdcf67
LZugar <59911931+LZugar@users.noreply.github.com>
2022-04-18 21:20:47 +0200

    Created HTML frame and started CSS coding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
       <h2>Weather app</h2>
+      <Weather />
       <footer>This project was created by Saario and is  
       <a href='https://github.com/Saario91/react-weather-project' target="_blank" rel="noopner">
         OpenSourced on GitHub</a>
