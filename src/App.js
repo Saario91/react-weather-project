d9b1771fcef66c5c52d528d8c5e839e11653b1a5
LZugar <59911931+LZugar@users.noreply.github.com>
2022-04-18 20:32:14 +0200

    Added github repository URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="App">
       Weather app
+      <footer>This project was created by Saario and is  
+      <a href='https://github.com/Saario91/react-weather-project' target="_blank">
+        OpenSourced on GitHub</a>
+      </footer>
     </div>
   );
 }
