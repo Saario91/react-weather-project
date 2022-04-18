122be3f5ab7220969697ef454344cff17e32f77d
LZugar <59911931+LZugar@users.noreply.github.com>
2022-04-18 20:53:33 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <div className='container'>
+      <h2>Weather app</h2>
       <footer>This project was created by Saario and is  
-      <a href='https://github.com/Saario91/react-weather-project' target="_blank">
+      <a href='https://github.com/Saario91/react-weather-project' target="_blank" rel="noopner">
         OpenSourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
